3fbc370696a55e6eb2aefe8476966a70d511fc45
DipokalLab <hhj@devent.kr>
2021-08-29 16:26:09 +0900

    feat: 파일 업로드

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
+upload_dir = './uploads/'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/upload')
+@app.route('/upload', methods=['POST'])
 def upload():
-    return render_template('index.html')
+    if request.method == 'POST':
+        file = request.files['file']
+        file.save(secure_filename(upload_dir+file.filename))
+        return jsonify(
+            status= 1,
+            filename= file.filename
+        )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8823) 
